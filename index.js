b22a3f539dfdc6b005686161d0b3b6cc2d677718
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-04-12 13:52:14 -0400

    commiting chnages to index.js

--- index.js
+++ index.js
@@ -1,46 +1,44 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const jest = require('jest');
-import inquirer from 'inquirer';
-const questions = [{
+const Triangle = require('./Assets/triangle.svg');
+const Square = require('./Assets/square.svg')
+const Circle = require('./Assets/circle.svg')
 
 
-    inquirer
-  .prompt([
-        /* Pass your questions in here */
-    ])
-        .then((answers) => {
-            // Use user feedback for... whatever!!
-        })
-        .catch((error) => {
-            if (error.isTtyError) {
-                // Prompt couldn't be rendered in the current environment
-            } else {
-                // Something else went wrong
-            }
-        });
-  {
-    type: 'input',
-    message: "Enter text for the logo."
-name: 'text',
 
-},
-{
-    type: 'checkbox',
-    message: "what color would you like the logo to be?",
-    choices: ["Green", "Turquoise", "Bisque"],
-    name: 'color',
-},
-{
-    type: 'checkbox',
-    message: 'what shape would you like you SVG logo to be?',
-    choices: ['Triangle', 'Square', 'Circle'],
-    name: 'shape',
+const questions = [
+    {
+        type: 'input',
+        message: "Enter text for the logo.",
+        name: 'text',
 
-},
+    },
+    {
+        type: 'checkbox',
+        message: "what color would you like the logo to be?",
+        choices: ["Green", "Turquoise", "Bisque"],
+        name: 'color',
+    },
+    {
+        type: 'checkbox',
+        message: 'what shape would you like you SVG logo to be?',
+        choices: ['Triangle', 'Square', 'Circle'],
+        name: 'shape',
+
+    },
+    {
+        type: 'checkbox',
+        message: 'What color would you like the text to be?',
+        choices: ['Triangle', 'Square', 'Circle'],
+        name: 'textColor',
+
+    },
 
 ];
-
+.then(()) =>
+.then(()) =>
+.then(()) =>
 
 
 
@@ -63,28 +61,31 @@ function writeFile() {
 };
 
 class triangle {
-    constructor(text, color, shape) {
+    constructor(text, color, shape, textColor) {
         this.text = text;
         this.color = color;
         this.shape = shape;
+        this.textColor = textColor;
     }
 
 }
 const triangle = new triangle;
 class Square {
-    constructor(text, color, shape) {
+    constructor(text, color, shape, textColor) {
         this.text = text;
         this.color = color;
         this.shape = shape;
+        this.textColor = textColor;
     }
 
 }
 const Square = new Square;
 class Circle {
-    constructor(text, color, shape) {
+    constructor(text, color, shape, textColor) {
         this.text = text;
         this.color = color;
         this.shape = shape;
+        this.textColor = textColor;
     }
 }
 const Circle = new Circle;
@@ -99,31 +100,31 @@ const Circle = new Circle;
 
 
 //Triangle
-<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+/* <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
 <polygon points="150, 18 244, 182 56, 182" fill="${Color}" />
 
-<text x="150" y="150" font-size="60" text-anchor="middle" fill="#3a0467">${text}</text>
+<text x="150" y="150" font-size="60" text-anchor="middle" fill="${Textcolor}">${text}</text>
 
-</svg>
+</svg> */
 
 //Square
 
-<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+/* <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
 <rect x="90" y="40" width="120" height="120" fill="${color}" />
 
 <text x="150" y="125" font-size="60" text-anchor="middle" fill="#444">${LogoText}</text>
 
-</svg>
+</svg> */
 
 
 
 //Circle
-<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+/* <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
   <polygon points="150, 18 244, 182 56, 182" fill="${Color}" />
 
   <text x="150" y="150" font-size="60" text-anchor="middle" fill="#3a0467">${text}</text>
 
-</svg>
+</svg> */
