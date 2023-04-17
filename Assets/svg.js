f68efc02fc28c0ee5f644904b421a748ae4de9e4
Wyatt Howlett <wyatthowlett@yahoo.com>
2023-04-17 17:33:07 -0400

    fixing logo rendering

--- Assets/svg.js
+++ Assets/svg.js
@@ -5,7 +5,7 @@ this.shapeEl = '';
 
     }
     render(){
-        return`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.textEl} ${this.shapeEl} </svg>`
+        return`<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${this.shapeEl} ${this.textEl} </svg>`
     }
     setText(message, color){
         if(message.length > 3) {
--- index.js
+++ index.js
@@ -15,28 +15,28 @@ function init() {
 
         },
         {
-            type: 'checkbox',
+            type: 'list',
             message: "what color would you like the logo to be?",
             choices: ["Green", "Turquoise", "Bisque"],
             name: 'color',
         },
         {
-            type: 'checkbox',
+            type: 'list',
             message: 'what shape would you like you SVG logo to be?',
             choices: ['Triangle', 'Square', 'Circle'],
             name: 'shapeType',
 
         },
         {
-            type: 'input',
+            type: 'list',
             message: 'What color would you like the text to be?',
-            // choices: ['White', 'Black', 'Blue'],
+            choices: ['White', 'Black', 'Blue'],
             name: 'textColor',
 
         },
 
     ])
-    
+
         .then(({ text, color, shapeType, textColor }) => {
             console.log(text, textColor);
             let shape;
@@ -67,88 +67,3 @@ function init() {
 init();
 
 
-// const shape = new Triangle();
-// shape.setColor("blue");
-// expect(shape.render()).toEqual('<polygon points="150, 18 244, 182 56, 182" fill="blue" />');
-
-
-
-
-// function writeFile() {
-//     inquirer
-//         .prompt(questions)
-//         .then((data) => {
-//             console.log(data)
-//             fs.writeFile("shape.svg", createSVG(data), (err) =>
-//                 err ? console.log(err) : console.log("Enjoy Your Logo"))
-//         });
-// };
-
-// class triangle {
-//     constructor(text, color, shape, textColor) {
-//         this.text = text;
-//         this.color = color;
-//         this.shape = shape;
-//         this.textColor = textColor;
-//     }
-
-// }
-// const triangle = new triangle;
-// class Square {
-//     constructor(text, color, shape, textColor) {
-//         this.text = text;
-//         this.color = color;
-//         this.shape = shape;
-//         this.textColor = textColor;
-//     }
-
-// }
-// const Square = new Square;
-// class Circle {
-//     constructor(text, color, shape, textColor) {
-//         this.text = text;
-//         this.color = color;
-//         this.shape = shape;
-//         this.textColor = textColor;
-//     }
-// }
-// const Circle = new Circle;
-
-
-
-
-
-
-
-
-
-
-//Triangle
-/* <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
-<polygon points="150, 18 244, 182 56, 182" fill="${Color}" />
-
-<text x="150" y="150" font-size="60" text-anchor="middle" fill="${Textcolor}">${text}</text>
-
-</svg> */
-
-//Square
-
-/* <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
-<rect x="90" y="40" width="120" height="120" fill="${color}" />
-
-<text x="150" y="125" font-size="60" text-anchor="middle" fill="#444">${LogoText}</text>
-
-</svg> */
-
-
-
-//Circle
-/* <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
-
-  <polygon points="150, 18 244, 182 56, 182" fill="${Color}" />
-
-  <text x="150" y="150" font-size="60" text-anchor="middle" fill="#3a0467">${text}</text>
-
-</svg> */
